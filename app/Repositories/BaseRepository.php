4afb63779a83da07639e32c2f18aa7e01b6c29f2
RafaelH77 <rafael.henrique7@hotmail.com>
2023-06-09 14:59:51 -0300

    Implementação BaseRepository

---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\Interfaces\IRepository;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseRepository implements IRepository
+{
+    protected $entity;
+
+    public function __construct(Model $model)
+    {
+        $this->entity = $model;
+    }
+
+    /**
+     * Lista todos
+     * @return array
+     */
+    public function getAll()
+    {
+        return $this->entity->paginate();
+    }
+
+    /**
+     * Seleciona por ID
+     * @param int $id
+     * @return object
+     */
+    public function getById(int $id)
+    {
+        return $this->entity->where('id', $id)->first();
+    }
+
+    /**
+     * Cria
+     * @param array $data
+     * @return object
+     */
+    public function create(array $data)
+    {
+        return $this->entity->create($data);
+    }
+
+    /**
+     * Atualiza
+     * @param int $id
+     * @param array $data
+     * @return object
+     */
+    public function update(int $id, array $data)
+    {
+        return $this->entity->where('id', $id)->update($data);
+    }
+
+    /**
+     * Deleta
+     * @param int $id
+     * @return int
+     */
+    public function delete(int $id)
+    {
+        return $this->entity->where('id', $id)->delete();
+    }
+}
--- app/Repositories/ProdutoRepository.php
+++ app/Repositories/ProdutoRepository.php
@@ -1,69 +1,16 @@
 <?php
 
-
 namespace App\Repositories;
-
 
 use App\Repositories\Interfaces\IProdutoRepository;
 use App\Models\Produto;
 
-
-class ProdutoRepository implements IProdutoRepository
+class ProdutoRepository extends BaseRepository implements IProdutoRepository
 {
     protected $entity;
 
     public function __construct(Produto $produto)
     {
         $this->entity = $produto;
-    }
-
-    /**
-     * Lista todos
-     * @return array
-     */
-    public function getAll()
-    {
-        return $this->entity->paginate();
-    }
-
-    /**
-     * Seleciona por ID
-     * @param int $id
-     * @return object
-     */
-    public function getById(int $id)
-    {
-        return $this->entity->where('id', $id)->first();
-    }
-
-    /**
-     * Cria
-     * @param array $data
-     * @return object
-     */
-    public function create(array $data)
-    {
-        return $this->entity->create($data);
-    }
-
-    /**
-     * Atualiza
-     * @param int $id
-     * @param array $data
-     * @return object
-     */
-    public function update(int $id, array $data)
-    {
-        return $this->entity->where('id', $id)->update($data);
-    }
-
-    /**
-     * Deleta
-     * @param int $id
-     * @return int
-     */
-    public function delete(int $id)
-    {
-        return $this->entity->where('id', $id)->delete();
     }
 }
