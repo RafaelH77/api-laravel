3f64ad5de92ec46f31ed44175fc36dde80826d5c
RafaelH77 <rafael.henrique7@hotmail.com>
2023-06-09 15:31:35 -0300

    Implementando BaseService

---
+++ app/Services/BaseService.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Interfaces\IRepository;
+
+class BaseService
+{
+    protected $repository;
+
+    public function __construct(IRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @return $array
+     */
+    public function getAll()
+    {
+        return $this->repository->getAll();
+    }
+
+    /**
+     * @param int $id
+     * @return object|null
+     */
+    public function getById(int $id): ?object
+    {
+        return $this->repository->getById($id);
+    }
+
+    /**
+     * @param array $data
+     * @return object $object
+     */
+    public function create(array $data)
+    {
+        return $this->repository->create($data);
+    }
+
+    /**
+     * @param int $id
+     * @param array $data
+     * @return object $object
+     */
+    public function update(int $id, array $data)
+    {
+        $result = $this->repository->update($id, $data);
+        return $result > 0 ? response()->json(['message' => 'Updated']) : response()->json(['message' => 'Not Found'], 404) ;
+    }
+
+    /**
+     * @param int $id
+     * @return object $object
+     */
+    public function delete(int $id)
+    {
+        $result = $this->repository->delete($id);
+        return $result > 0 ? response()->json(['message' => 'Deleted']) : response()->json(['message' => 'Not Found'], 404) ;
+    }
+}
--- app/Services/ProdutoService.php
+++ app/Services/ProdutoService.php
@@ -1,64 +1,15 @@
 <?php
 
-
 namespace App\Services;
-
 
 use App\Repositories\Interfaces\IProdutoRepository;
 
-class ProdutoService
+class ProdutoService extends BaseService
 {
     protected $repository;
 
     public function __construct(IProdutoRepository $repository)
     {
         $this->repository = $repository;
-    }
-
-    /**
-     * @return $array
-     */
-    public function getAll()
-    {
-        return $this->repository->getAll();
-    }
-
-    /**
-     * @param int $id
-     * @return object|null
-     */
-    public function getById(int $id): ?object
-    {
-        return $this->repository->getById($id);
-    }
-
-    /**
-     * @param array $data
-     * @return object $object
-     */
-    public function create(array $data)
-    {
-        return $this->repository->create($data);
-    }
-
-    /**
-     * @param int $id
-     * @param array $data
-     * @return object $object
-     */
-    public function update(int $id, array $data)
-    {
-        $result = $this->repository->update($id, $data);
-        return $result > 0 ? response()->json(['message' => 'Updated']) : response()->json(['message' => 'Not Found'], 404) ;
-    }
-
-    /**
-     * @param int $id
-     * @return object $object
-     */
-    public function delete(int $id)
-    {
-        $result = $this->repository->delete($id);
-        return $result > 0 ? response()->json(['message' => 'Deleted']) : response()->json(['message' => 'Not Found'], 404) ;
     }
 }
