6aa2ca56ae3ac90e6e469c295233ee614b97d0a8
RafaelH77 <rafael.henrique7@hotmail.com>
2023-06-12 12:08:10 -0300

    Update 2023_06_12_145121_create_vendedors_table.php

--- database/migrations/2023_06_12_145121_create_vendedors_table.php
+++ database/migrations/2023_06_12_145121_create_vendedors_table.php
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('vendedors');
+        Schema::dropIfExists('vendedores');
     }
 };
