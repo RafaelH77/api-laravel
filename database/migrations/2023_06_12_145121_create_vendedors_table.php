5ce4d7b323b8268f0aa07e7da15e3d773997ec77
RafaelH77 <rafael.henrique7@hotmail.com>
2023-06-12 12:08:01 -0300

    Update 2023_06_12_145121_create_vendedors_table.php

--- database/migrations/2023_06_12_145121_create_vendedors_table.php
+++ database/migrations/2023_06_12_145121_create_vendedors_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('vendedors', function (Blueprint $table) {
+        Schema::create('vendedores', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('nome');
