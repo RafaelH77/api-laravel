2e2cf986e60adc44e19eb83c52d50b399f7018d8
RafaelH77 <rafael.henrique7@hotmail.com>
2023-06-16 08:21:07 -0300

    Implementação do controle dos itens dos pedidos

--- app/Services/PedidoService.php
+++ app/Services/PedidoService.php
@@ -2,10 +2,14 @@
 
 namespace App\Services;
 
+use App\Models\Pedido;
+use App\Models\PedidoItem;
 use App\Repositories\Interfaces\IPedidoRepository;
 use App\Repositories\Interfaces\IVendedorRepository;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
+use function PHPUnit\Framework\isNull;
 
 class PedidoService extends BaseService
 {
@@ -40,7 +44,13 @@ class PedidoService extends BaseService
 
         $comissao = $this->calculaComissao($data['valor']);
 
+        $itens = $data['itens'];
+        unset($data['itens']);
+
         $this->repository->update($id, array_merge($data, ['comissao' => $comissao]));
+
+        $result = $this->repository->getByIdWithVendedor($id);
+        $this->UpdateOrDeleteChildren($result->itens(), $itens);
 
         return $this->repository->getByIdWithVendedor($id);
     }
@@ -54,5 +64,37 @@ class PedidoService extends BaseService
 
     public function calculaComissao($valor){
         return round($valor * 0.1, 2);
+    }
+
+    protected function UpdateOrDeleteChildren($dbChildList, $childList)
+    {
+        if (is_null($childList))
+            return;
+
+        $childListIds = array_column($childList, 'id');
+
+        // Delete children
+        if (!is_null($dbChildList)){
+            $childListIdsDelete = array_column(array_filter($dbChildList->get()->toArray(), function($value) use($childListIds) {
+                return !in_array($value['id'], $childListIds);
+            }), 'id');
+
+            $dbChildList
+                ->whereIn('id', $childListIdsDelete)
+                ->delete();
+        }
+
+        // Update/Insert children
+        foreach ($childList as $child){
+            $existingChild = null;
+            if (array_key_exists('id', $child))
+                $existingChild = $dbChildList->where('id', '=', $child['id']);
+
+            if (!is_null($existingChild)) {
+                $existingChild->update($child);
+            } else {
+                $dbChildList->create($child);
+            }
+        }
     }
 }
